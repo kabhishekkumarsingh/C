c15ac96e91789deee1efeff9be6a051c62edbc44
Abhishek kumar singh <115399497+kabhishekkumarsingh@users.noreply.github.com>
2023-11-02 20:14:14 +0530

    Create digonal_element_3.c
    digonal element

---
+++ 2D-array/digonal_element_3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+int a[3][3];
+printf("Enter matrix element: ");
+for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        scanf("%d",&a[i][j]);
+    }
+}
+    printf("matrix\n");
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("Digonal element:\n ");
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(i==j || i+j==3-1)
+            printf("%d\n",a[i][j]);
+        }
+    }
+    
+  return 0;
+}
