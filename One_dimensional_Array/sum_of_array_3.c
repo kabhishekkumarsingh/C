76eadf3ba0132762ab7d34e0454d2d82503cf132
Abhishek kumar singh <115399497+kabhishekkumarsingh@users.noreply.github.com>
2023-11-02 15:43:39 +0530

    Create sum_of_array_3.c
    sum of array

---
+++ One_dimensional_Array/sum_of_array_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+int a[5],sum=0;
+printf("Enter elements: ");
+for(int i=0;i<5;i++){
+    scanf("%d",&a[i]);
+    
+}
+printf("Array elements are: ");
+for(int i=i;i<5;i++){
+    sum=sum+a[i];
+}
+printf("sum of array = %d",sum);
+    return 0;
+}
